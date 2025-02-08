23b8ad36fe896c8b84b9416671d55fd35a0b3dc8
MannyAdewumi <mannyadewumi@gmail.com>
2025-02-08 20:46:46 +0100

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,10 +12,11 @@ def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-       """returns HBNB"""
-          return 'HBNB'
+    """returns HBNB"""
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
